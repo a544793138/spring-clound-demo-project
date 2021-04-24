53a3dc905bd78df80b6e8df5ac4b93717d2762d1
TJwoods <544793138@qq.com>
2021-04-24 19:28:31 +0800

    添加使用 consul 注册中心的模块

---
+++ cloud-providerconsul-payment8006/src/main/java/com/atguigu/springcloud/PaymentMain8006.java
@@ -0,0 +1,15 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+// 该注解用于向使用 consul / zookeeper 作为注册中心时注册服务
+@EnableDiscoveryClient
+public class PaymentMain8006 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8006.class, args);
+    }
+}
