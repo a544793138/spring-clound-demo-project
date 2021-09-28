39de5283b5c8410b0f02cf5a4e1eab6ae151f6ad
TJwoods <544793138@qq.com>
2021-09-28 23:47:10 +0800

    添加 hystrix 案例的模块

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/controller/OrderHystrixController.java
@@ -1,6 +1,8 @@
 package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,8 +21,14 @@ public class OrderHystrixController {
     }
 
     @GetMapping("/consumer/payment/hystrix/service-timeout/{id}")
+    @HystrixCommand(fallbackMethod = "serviceTimeoutFallback", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+    })
     public String serviceTimeout(@PathVariable("id") int id) {
         return paymentHystrixService.serviceTimeout(id);
     }
 
+    public String serviceTimeoutFallback(@PathVariable("id") int id) {
+        return String.format("consumer serviceTimeoutFallback :%s. Wait timeout.", id);
+    }
 }
