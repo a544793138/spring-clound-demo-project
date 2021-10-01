25d9037c4c9658fd52aa1cf47e5cddb973e83a42
TJwoods <544793138@qq.com>
2021-10-01 14:19:58 +0800

    添加 hystrix 熔断

--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
@@ -5,6 +5,8 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 @Service
 @Slf4j
 public class PaymentService {
@@ -28,6 +30,34 @@ public class PaymentService {
 
     public String serviceTimeoutFallback(int id) {
         return String.format("serviceTimeoutFallback :%s. Wait timeout.", id);
+    }
+
+    @HystrixCommand(fallbackMethod = "serviceCircuitBreakerFallback", commandProperties = {
+            // 是否启用断路器机制
+            @HystrixProperty(name = "circuitBreaker.enabled", value = "true"),
+            // 设置在滚动时间窗口期内，断路器发送熔断的最小请求次数。
+            // 只有在滚动时间窗口期内，且达到这个请求次数后，才开始判断这些请求里的失败率是否达到设置的值，如果达到才开启断路器
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),
+            // 休眠时间窗口期。表示断路器开启后，经过这个时间后，断路器会切换到“半开”状体，尝试放行请求命令，如果依旧失败，断路器就切换回开启状态，如果成功，则关闭断路器，恢复调用
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000"),
+            // 设置滚动时间窗口期内，一定请求次数下的最高失败率。达到这个失败率后，就会发送熔断
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60"),
+            // 设置滚动时间窗口期
+            @HystrixProperty(name = "metrics.rollingStats.timeInMilliseconds", value = "10000")
+    })
+    public String serviceCircuitBreaker(int id) {
+        if (id < 0) {
+            throw new IllegalArgumentException("id 不能为复数");
+        }
+        return String.format("serviceCircuitBreaker: %s, threadName: %s", id, Thread.currentThread().getName());
+    }
+
+    public String serviceCircuitBreakerFallback(int id, Exception e) {
+        String message = "";
+        if (Objects.nonNull(e)) {
+            message = e.getMessage();
+        }
+        return String.format("serviceCircuitBreakerFallback ：exception message: %s, id: %s.", message, id);
     }
 
 }
