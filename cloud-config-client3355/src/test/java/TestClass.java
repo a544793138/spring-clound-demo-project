7d7c6a4498d0447220739e49332a52d84eeada9b
TJwoods <544793138@qq.com>
2022-08-10 00:14:54 +0800

    为 Config 添加消息总线 Bus

---
+++ cloud-config-client3355/src/test/java/TestClass.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import java.util.UUID;
+
+public class TestClass {
+
+    @Test
+    public void test() {
+        System.out.println(UUID.randomUUID().toString());
+    }
+}
